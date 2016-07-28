1759acc5320d18e472480fd6d5013a276f5d4fa2
Alison <alisonhmullin@gmail.com>
2016-07-28 19:55:57 -0400

    build anagram detector

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+def match(array)
+  finalwords = []
+  array.each do |item|
+    if item.split("").sort == self.word.split("").sort
+      finalwords << item
+    end
+  end
+  finalwords
+end
+
+end
